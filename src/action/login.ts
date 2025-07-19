8486e21f01bfa595741fb7d5c9db19d4f4bedf24
KLproduction <kent.law.production01@gmail.com>
2025-07-19 19:51:22 +0100

    Add crypto import to login action

--- src/action/login.ts
+++ src/action/login.ts
@@ -4,6 +4,7 @@ import { db } from "@/lib/db";
 import { createClient } from "@/utils/supabase/client";
 import { createClientServer } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
+import crypto from "crypto";
 
 export const GitHubLogin = async () => {
   const supabase = createClient();
